8a214bbe0817c3e3bc423e5ea035a37c9abddee8
Igboanugwo Collins <munisco12@gmail.com>
2021-05-03 09:12:34 +0100

    Added the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,10 @@
-
+import Image from 'next/image'
 
 const Header = () => {
 
     return (
         <div>
-            <Image src='https://links.papareact.com/ua6' width='' height=''/>
+            <Image src='https://links.papareact.com/ua6' width={200} height={100}/>
         </div>
     )
 }
