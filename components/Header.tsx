116a1941ee33d30c2f558d7c7a29b039ffa6388c
Igboanugwo Collins <munisco12@gmail.com>
2021-05-03 09:45:51 +0100

    Added the images in next config

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,16 @@
 import Image from 'next/image'
+import HeaderItem from './HeaderItem'
+
+import {BadgeCheckIcon, CollectionIcon, Homeicon, LightningBoltIcon, SearchIcon, UserIcon}
 
 const Header = () => {
 
     return (
         <header>
+            <div>
+                <HeaderItem title='Home' Icon={}/>
+            </div>
+
             <Image src='https://links.papareact.com/ua6' width={200} height={100} className='object-contain'/>
         </header>
     )
