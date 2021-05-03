901a60bc0bbdd66c5055a67c0c92de2783523879
Igboanugwo Collins <munisco12@gmail.com>
2021-05-03 19:58:47 +0100

    removed heroicons because it was giving issures

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,9 @@ import Image from 'next/image'
 import HeaderItem from './HeaderItem'
 
 
+// import {BadgeCheckOutline, CollectionOutline, HomeOutline, LightningBoltOutline, SearchOutline, UserOutline} from '@graywolfai/react-heroicons'
 
-import {AiFillAccountBook} from 'react-icons/ai'
+import {HiOutlineCollection} from 'react-icons/hi'
 
 const Header = () => {
 
@@ -12,6 +13,7 @@ const Header = () => {
             <div>
                 {/* <BadgeCheckOutline/> */}
                 <AiFillAccountBook/>
+                <HeaderItem title='Home' Icon={HiOutlineCollection}/>
             </div>
 
             <Image src='https://links.papareact.com/ua6' width={200} height={100} className='object-contain'/>
