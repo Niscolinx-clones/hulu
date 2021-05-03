4a4dfdc043aa7f2089eee35f37deca62fb1efc12
Igboanugwo Collins <munisco12@gmail.com>
2021-05-03 07:51:04 +0100

    working on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
 import Link from 'next/link'
-
+import Head from 'next/head'
 const IndexPage = () => (
   <>
-    <title>hello</title>
+    <Head>
+      <title>Hulu clone</title>
+      
+    </Head>
     <h1>The Home page 👋</h1>
     <p>
       <Link href="/about">
         <a>Go to About page</a>
       </Link>
     </p>
-  </>
+  </title>
 )
 
 export default IndexPage
