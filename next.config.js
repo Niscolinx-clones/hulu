35e939f6bd7b6253550ff49136de0ea5d9f698d8
Igboanugwo Collins <munisco12@gmail.com>
2021-05-03 09:16:59 +0100

    Added the images in next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     images: {
-        domains: ['https://links.papareact.com/ua6', 'image.tmdb.orgg']
+        domains: ['https://links.papareact.com/ua6', 'image.tmdb.org']
     }
 }
