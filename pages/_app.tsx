1889533b242b48b6a19ff7d3423b04a49ec7c56e
Igboanugwo Collins <munisco12@gmail.com>
2021-05-04 06:01:38 +0100

    imported tailwind into my app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { AppProps } from 'next/app'
 
 import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
 
-function MyApp({ Component, pageProps }: any) {
+
+function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />
 }
 
