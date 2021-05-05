44b6be2ad00b8221ece6056afdcfa02f2fc71239
Igboanugwo Collins <munisco12@gmail.com>
2021-05-05 07:26:44 +0100

    working on header

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,17 +4,21 @@ import HeaderItem from './HeaderItem'
 
 // import {, HomeOutline, LightningBoltOutline, SearchOutline, UserOutline} from '@graywolfai/react-heroicons'
 
-import {HiOutlineCollection} from 'react-icons/hi'
+import {HiOutlineCollection, HiOutlineLightningBolt} from 'react-icons/hi'
 import {BiBadgeCheck, BiSearch} from 'react-icons/bi'
 import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai'
 
 const Header = () => {
 
     return (
-        <header>
-            <div>
-                {/* <BadgeCheckOutline/> */}
-                <HeaderItem title='Home' Icon={HiOutlineCollection}/>
+        <header className='flex flex-col'>
+            <div className='flex flex-grow justify-evenly max-w-2xl'>
+                <HeaderItem title='Home' Icon={AiOutlineHome}/>
+                <HeaderItem title='Trending' Icon={HiOutlineLightningBolt}/>
+                <HeaderItem title='Verified' Icon={BiBadgeCheck}/>
+                <HeaderItem title='Collections' Icon={HiOutlineCollection}/>
+                <HeaderItem title='Search' Icon={BiSearch}/>
+                <HeaderItem title='Account' Icon={AiOutlineUser}/>  
               
             </div>
 
