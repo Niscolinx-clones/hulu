e60ee448b7dfbb3ec7e3d4d8c564399af5a6c34f
Igboanugwo Collins <munisco12@gmail.com>
2021-05-05 17:00:33 +0100

    added the the Nav bar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,19 +1,23 @@
 import request from "../utils/request"
 
-interface TmdbRequestArray {
 
-    title: string
-    url: string
-    
+interface TmdbRequestArray {
+    [index: number]: [
+        string,
+        {
+            title: string;
+            url: string
+        }
+    ]
+   
 }
 const Nav = () => {
     console.log(Object.entries(request))
     return (
         <nav>
-            {Object.entries(request).map(([key, {title, url}]) => (
-                
-                <h2>{title}</h2>
-            ))  }
+            {Object.entries(request).map(([key, {title, url}])  => (
+                <p>{title}</p>
+            ))}
         </nav>
     )
 }
