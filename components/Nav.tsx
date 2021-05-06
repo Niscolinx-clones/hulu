f35d3d8ed275b2f1a64b1acb003546a00387a909
Igboanugwo Collins <munisco12@gmail.com>
2021-05-06 14:20:16 +0100

    working on nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,6 +3,7 @@ import request from '../utils/request'
 
 interface TmdbRequestArray {
     [index: number]: [
+        string,
         {
             title: string
             url: string
@@ -13,9 +14,9 @@ const Nav = () => {
     console.log(Object.entries(request))
     return (
         <nav>
-            {Object.entries(request).map(([{ title, url }]:[]TmdbRequestArray) => {
-                return (<p>{title}</p>)
-            })}
+            {Object.entries(request).map(([key, { title, url }]) => 
+                (<p>{title}</p>)
+            )}
         </nav>
     )
 }
