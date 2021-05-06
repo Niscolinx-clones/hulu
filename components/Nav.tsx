571de561287376cb26b25213e9e82266a1232e6d
Igboanugwo Collins <munisco12@gmail.com>
2021-05-06 15:08:23 +0100

    working on nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -13,11 +13,11 @@ interface TmdbRequestArray {
 const Nav = () => {
     return (
         <nav>
-            <div className='flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll '>
+            <div className='flex sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide'>
                 {Object.entries(request).map(([key, { title, url }]) => (
                     <h2
                         key={key}
-                        className='cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500'
+                        className='last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500'
                     >
                         {title}
                     </h2>
