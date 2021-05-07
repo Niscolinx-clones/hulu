917c53c4aa11cf546490a984f94a0ca94a899cb0
Igboanugwo Collins <munisco12@gmail.com>
2021-05-07 07:44:34 +0100

    Working on server side props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import Head from 'next/head'
 import { GetServerSideProps } from 'next'
+import { AppProps } from 'next/app'
+
 
 import Header from '../components/Header'
 import Nav from '../components/Nav'
 import Results from '../components/Results'
 import requests from '../utils/request'
 
-const IndexPage = (props) => {
+const IndexPage = (props: AppProps) => {
     console.log(props)
     return (
         <>
