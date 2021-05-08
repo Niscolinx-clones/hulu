31c2c9267441cb569f35bfa49c4ea80706380350
Igboanugwo Collins <munisco12@gmail.com>
2021-05-08 09:09:50 +0100

    Working on thumbnail

--- components/Results.tsx
+++ components/Results.tsx
@@ -3,9 +3,9 @@ import Thumbnail from './Thumbnail'
 
 const Results = ({ results }: IndexProps) => {
     return (
-        <div className='px-5 my-10 sm:grid'>
+        <div className='px-5 my-10 sm:grid md:grid-cols-2'>
             {results.map((result: Iresult) => {
-                return <Thumbnail result={result} />
+                return <Thumbnail key={result.id} result={result} />
             })}
         </div>
     )
--- interfaces/results.ts
+++ interfaces/results.ts
@@ -1,5 +1,12 @@
+
 export interface Iresult {
-    [key: string]: string | number | boolean | Array<string> | Array<number>
+    [key: string]:
+        | string
+        | number
+        | boolean
+        | Array<string>
+        | Array<number>
+        | null
 }
 export interface IndexProps {
     results: Array<Iresult>
