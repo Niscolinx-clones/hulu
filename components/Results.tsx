4e8af49eeb01ff5616a06e7f1820cccc8d8b06b7
Igboanugwo Collins <munisco12@gmail.com>
2021-05-08 08:59:29 +0100

    Working on thumbnail

--- components/Results.tsx
+++ components/Results.tsx
@@ -1,12 +1,14 @@
-import { IndexProps, Iresult } from "../interfaces/results"
-import Thumbnail from "./Thumbnail"
+import { IndexProps, Iresult } from '../interfaces/results'
+import Thumbnail from './Thumbnail'
 
-const Results = ({results}: IndexProps) => {
-    return (<div>
-        {results.map((result: Iresult) => {
-            return <Thumbnail result={result}/>
-        })}
-    </div>)
+const Results = ({ results }: IndexProps) => {
+    return (
+        <div className='px-5 my-10 sm:grid'>
+            {results.map((result: Iresult) => {
+                return <Thumbnail result={result} />
+            })}
+        </div>
+    )
 }
 
 export default Results
--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -10,7 +10,7 @@ const Thumbnail = ({ result }: IThumbnail) => {
     const BASE_URL = 'https://image.tmdb.org/t/p/original/'
     
     return (
-        <div className='group cursor-pointer'>
+        <div className='group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'>
             <Image
                 layout='responsive'
                 height={1000}
