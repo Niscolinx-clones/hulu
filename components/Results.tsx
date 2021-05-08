2358ba12731e791094f96fe5ef0d2f07cda4665c
Igboanugwo Collins <munisco12@gmail.com>
2021-05-08 09:32:54 +0100

    Working on thumbnail

--- components/Results.tsx
+++ components/Results.tsx
@@ -3,7 +3,7 @@ import Thumbnail from './Thumbnail'
 
 const Results = ({ results }: IndexProps) => {
     return (
-        <div className='px-5 my-10 sm:grid md:grid-cols-2 lg'>
+        <div className='px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 xl'>
             {results.map((result: Iresult, key: number) => {
                 return <Thumbnail key={key} result={result} />
             })}
