b462e7e127f896a38ea4fd884034340241b49215
Igboanugwo Collins <munisco12@gmail.com>
2021-05-08 17:16:59 +0100

    Working on thumbnail

--- components/Results.tsx
+++ components/Results.tsx
@@ -8,7 +8,7 @@ const Results = ({ results }: IndexProps) => {
     return (
         <FlipMove className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center'>
             {results.map((result: Iresult, key: number) => {
-                return <Thumbnail key={key} result={result} />
+                return <Thumbnail key={result.id} result={result} />
             })}
         </FlipMove>
     )
