46a1edd46c1be07c65bf3277e814f177e7eb54a8
Igboanugwo Collins <munisco12@gmail.com>
2021-05-08 09:26:55 +0100

    Working on thumbnail

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,10 @@ import { AppProps } from 'next/app'
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
 
+// "autoprefixer": "^10.0.4",
+// "postcss": "^8.1.10",
+// "tailwindcss": "^2.1.1",
+
 
 function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />
